94fdfbdb6099b385c11ed4b49b2766ede334ef07
Yusuf Akyüz <yusufakyus47@gmail.com>
2023-09-07 11:44:28 +0300

    Layout Written

---
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.calculator.databinding.ActivityMainBinding;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    
+}
